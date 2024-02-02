e55a4140e4ffe4b55e18e19eda0941bbf3860525
Sherlock Holo <sherlockya@gmail.com>
2024-02-03 00:46:25 +0800

    test: add write test

--- src/write.rs
+++ src/write.rs
@@ -5,22 +5,21 @@ use std::{io, slice};
 
 use compio_buf::{BufResult, IntoInner, IoBuf, IoBufMut};
 use futures_util::task::AtomicWaker;
-use futures_util::FutureExt;
 
 type WriteFut<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> =
     impl Future<Output = (Io, BufResult<usize, Buf>)> + 'a + Unpin;
 
-type FlushFut<'a, Io: compio_io::AsyncWrite + Unpin + 'a> =
+type FlushFut<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> =
     impl Future<Output = (Io, io::Result<()>)> + 'a + Unpin;
 
-type CloseFut<'a, Io: compio_io::AsyncWrite + Unpin + 'a> =
+type CloseFut<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> =
     impl Future<Output = (Io, io::Result<()>)> + 'a + Unpin;
 
 enum FutState<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> {
     Idle,
     Write(WriteFut<'a, Io, Buf>),
-    Flush(FlushFut<'a, Io>),
-    Close(CloseFut<'a, Io>),
+    Flush(FlushFut<'a, Io, Buf>),
+    Close(CloseFut<'a, Io, Buf>),
 }
 
 pub struct CompatWrite<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> {
@@ -69,14 +68,11 @@ impl<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> futures_
                         buf.copy_from_slice(&data[..size]);
                     }
 
-                    this.fut = FutState::Write(
-                        async move {
-                            let BufResult(res, buf) = io.write(buf).await;
+                    this.fut = FutState::Write(Box::pin(async move {
+                        let BufResult(res, buf) = io.write(buf).await;
 
-                            (io, BufResult(res, buf.into_inner()))
-                        }
-                        .boxed_local(),
-                    );
+                        (io, BufResult(res, buf.into_inner()))
+                    }));
                 }
 
                 FutState::Write(ref mut fut) => {
@@ -118,14 +114,11 @@ impl<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> futures_
                 FutState::Idle => {
                     let mut io = this.io.take().unwrap();
 
-                    this.fut = FutState::Flush(
-                        async move {
-                            let res = io.flush().await;
+                    this.fut = FutState::Flush(Box::pin(async move {
+                        let res = io.flush().await;
 
-                            (io, res)
-                        }
-                        .boxed_local(),
-                    );
+                        (io, res)
+                    }));
                 }
 
                 FutState::Write(_) => {
@@ -163,14 +156,11 @@ impl<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> futures_
                 FutState::Idle => {
                     let mut io = this.io.take().unwrap();
 
-                    this.fut = FutState::Close(
-                        async move {
-                            let res = io.shutdown().await;
+                    this.fut = FutState::Close(Box::pin(async move {
+                        let res = io.shutdown().await;
 
-                            (io, res)
-                        }
-                        .boxed_local(),
-                    );
+                        (io, res)
+                    }));
                 }
 
                 FutState::Write(_) => {
@@ -199,5 +189,66 @@ impl<'a, Io: compio_io::AsyncWrite + Unpin + 'a, Buf: IoBufMut + Unpin> futures_
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use compio::net::{TcpListener, TcpStream, UnixListener, UnixStream};
+    use compio::runtime;
+    use compio::runtime::Runtime;
+    use compio_io::AsyncReadExt;
+    use futures_util::AsyncWriteExt;
+    use tempfile::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn test_tcp_write() {
+        Runtime::new().unwrap().block_on(async {
+            let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+            let addr = listener.local_addr().unwrap();
+
+            runtime::spawn(async move {
+                let tcp_stream = listener.accept().await.unwrap().0;
+                let mut compat_write = CompatWrite::new(tcp_stream, vec![0; 100]);
+
+                compat_write.write_all(b"test").await.unwrap();
+            })
+            .detach();
+
+            let mut tcp_stream = TcpStream::connect(addr).await.unwrap();
+
+            let buf = vec![0; 4];
+            let (n, buf) = tcp_stream.read_exact(buf).await.unwrap();
+
+            assert_eq!(&buf[..n], b"test");
+        });
+    }
+
+    #[test]
+    fn test_udp_read() {
+        Runtime::new().unwrap().block_on(async {
+            let dir = TempDir::new_in(env::temp_dir()).unwrap();
+            let path = dir.path().join("test.sock");
+            let unix_listener = UnixListener::bind(&path).unwrap();
+
+            runtime::spawn(async move {
+                let unix_stream = unix_listener.accept().await.unwrap().0;
+                let mut compat_write = CompatWrite::new(unix_stream, vec![0; 100]);
+
+                compat_write.write_all(b"test").await.unwrap();
+            })
+            .detach();
+
+            let mut unix_stream = UnixStream::connect(path).unwrap();
+
+            let buf = vec![0; 4];
+            let (n, buf) = unix_stream.read_exact(buf).await.unwrap();
+
+            assert_eq!(&buf[..n], b"test");
+        });
     }
 }
